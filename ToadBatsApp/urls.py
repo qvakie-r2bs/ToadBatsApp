b2a59c20240c5845db746c6180e9390dac82fdad
jakesan <81538424+jakesan@users.noreply.github.com>
2022-06-06 18:07:27 +0300

    ok

--- ToadBatsApp/urls.py
+++ ToadBatsApp/urls.py
@@ -7,5 +7,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls,),
-    path('', include('toadbat.urls'))
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('toadbat.urls')),
+    path('', include('base.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
